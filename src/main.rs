b07ab9ee7cbb59a8d1925b0bc15269cb1a3eadff
Axel Örn Sigurðsson <git@absalon.dev>
2025-02-15 19:04:11 +0100

    Add hardcoded PONG response

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use thiserror::Error;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+type Result<T> = std::result::Result<T, RustisError>;
+
+#[derive(Error, Debug)]
+enum RustisError {
+    #[error("IO error: {0}")]
+    IoError(#[from] std::io::Error),
+}
+
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                let mut buf = [0; 512];
+                let _ = stream.read(&mut buf)?;
+                println!("Request: {}", String::from_utf8_lossy(&buf[..]));
+
+                let response = b"+PONG\r\n";
+                let _ = stream.write(&response[..])?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
