d9a5f2c901fb5ff60e3a4a1d9d337be534f6f5d5
Axel Örn Sigurðsson <git@absalon.dev>
2025-03-17 21:36:05 +0100

    Add support for reading size encoded values

--- src/parsers/rdb.rs
+++ src/parsers/rdb.rs
@@ -47,6 +47,95 @@ fn nom_rdb_op_code(input: &[u8]) -> IResult<&[u8], OpCode> {
     Ok((input, op_code))
 }
 
+/// Parse size encoding
+///
+/// If the first two bits are 0b00:
+///     The size is the remaining 6 bits of the byte
+///     Example: 0b00001010 -> 10
+///
+/// If the first two bits are 0b01:
+///     The size is the next 14 bits, in big-endian
+///     Example: 0b01000010 10111100 -> 700
+///
+/// If the first two bits are 0b11:
+///     The size is in little endian, either 8, 16 or 32 bits
+///     If the first byte is 0xC0 (0b11000000): 8-bits
+///     If the first byte is 0xC1 (0b11000001): 16-bits
+///     If the first byte is 0xC2 (0b11000010): 32-bits
+///     If the first byte is 0xC3 (0b11000011): LZF-compressed (not supported)
+///
+/// If the first 8 bits are 0b10000000:
+///     Ignore the remaining 6 bits of the first byte,
+///     size is 32 bit (next 4 bytes), big endian:
+///     Example: 0b10000000 00000000 00000000 01000010 01101000 -> 17000
+///
+/// If the first 8 bits are 0b10000001:
+///     Ignore the remaining 6 bits of the first byte,
+///     size is 64 bit (next 8 bytes), big endian:
+///
+///
+/// TODO: Do I need to support the output being 8u/u16/u32/u64 with an enum?
+fn nom_size_encoding(input: &[u8]) -> IResult<&[u8], usize> {
+    let (input, first_byte) = take(1usize).parse(input)?;
+
+    match first_byte[0] >> 6 {
+        0b00 => Ok((input, first_byte[0] as usize & 0b0011_1111)),
+        0b01 => {
+            let (input, second_byte) = take(1usize).parse(input)?;
+            Ok((
+                input,
+                ((first_byte[0] as usize & 0b0011_1111) << 8) | second_byte[0] as usize,
+            ))
+        }
+        0b10 if first_byte[0] == 0b10000000 => {
+            let (input, bytes) = take(4usize).parse(input)?;
+            Ok((
+                input,
+                u32::from_be_bytes(bytes.try_into().unwrap()) as usize,
+            ))
+        }
+        0b10 if first_byte[0] == 0b10000001 => {
+            let (input, bytes) = take(8usize).parse(input)?;
+            Ok((
+                input,
+                u64::from_be_bytes(bytes.try_into().unwrap()) as usize,
+            ))
+        }
+        0b11 if first_byte[0] == 0b11000000 => {
+            let (input, bytes) = take(1usize).parse(input)?;
+            Ok((input, u8::from_le_bytes(bytes.try_into().unwrap()) as usize))
+        }
+        0b11 if first_byte[0] == 0b11000001 => {
+            let (input, bytes) = take(2usize).parse(input)?;
+            Ok((
+                input,
+                u16::from_le_bytes(bytes.try_into().unwrap()) as usize,
+            ))
+        }
+        0b11 if first_byte[0] == 0b11000010 => {
+            let (input, bytes) = take(4usize).parse(input)?;
+            Ok((
+                input,
+                u32::from_le_bytes(bytes.try_into().unwrap()) as usize,
+            ))
+        }
+        0b11 if first_byte[0] == 0b11000011 => {
+            // LZF-compressed
+            unimplemented!()
+        }
+        _ => unreachable!(),
+    }
+}
+
+/// Parse size-encoded string
+///
+/// Note: We work with values as &[u8], that includes strings
+fn nom_size_encoded_string(input: &[u8]) -> IResult<&[u8], &[u8]> {
+    let (input, size) = nom_size_encoding(input)?;
+    let (input, string) = take(size).parse(input)?;
+    Ok((input, string))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,5 +157,70 @@ mod tests {
         assert_eq!(nom_rdb_op_code(&[0xFD]), Ok((&b""[..], OpCode::EXPIRETIME)));
         assert_eq!(nom_rdb_op_code(&[0xFE]), Ok((&b""[..], OpCode::SELECTDB)));
         assert_eq!(nom_rdb_op_code(&[0xFF]), Ok((&b""[..], OpCode::EOF)));
+    }
+
+    #[test]
+    fn test_nom_size_encoding_6_bits() {
+        assert_eq!(nom_size_encoding(&[0b00001010]), Ok((&b""[..], 10)));
+        assert_eq!(nom_size_encoding(&[0x0D]), Ok((&b""[..], 13)));
+    }
+
+    #[test]
+    fn test_nom_size_encoding_14_bits() {
+        assert_eq!(
+            nom_size_encoding(&[0b01000010, 0b10111100]),
+            Ok((&b""[..], 700))
+        );
+    }
+
+    #[test]
+    fn test_nom_size_encoding_32_bits() {
+        assert_eq!(
+            nom_size_encoding(&[0b10000000, 0b00000000, 0b00000000, 0b01000010, 0b01101000]),
+            Ok((&b""[..], 17000))
+        );
+    }
+
+    #[test]
+    fn test_nom_size_encoding_64_bits() {
+        assert_eq!(
+            nom_size_encoding(&[
+                0b10000001, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000,
+                0b01000010, 0b01101000
+            ]),
+            Ok((&b""[..], 17000))
+        );
+    }
+
+    #[test]
+    fn test_nom_size_encoding_string_8_bits() {
+        assert_eq!(nom_size_encoding(&[0xC0, 0x7B]), Ok((&b""[..], 123)));
+    }
+
+    #[test]
+    fn test_nom_size_encoding_string_16_bits() {
+        assert_eq!(
+            nom_size_encoding(&[0xC1, 0x39, 0x30]),
+            Ok((&b""[..], 12345))
+        );
+    }
+
+    #[test]
+    fn test_nom_size_encoding_string_32_bits() {
+        assert_eq!(
+            nom_size_encoding(&[0xC2, 0x87, 0xD6, 0x12, 0x00]),
+            Ok((&b""[..], 1234567))
+        );
+    }
+
+    #[test]
+    fn test_nom_string() {
+        let string = &b"Hello, world!"[..];
+        assert_eq!(
+            nom_size_encoded_string(&[
+                0x0D, b'H', b'e', b'l', b'l', b'o', b',', b' ', b'w', b'o', b'r', b'l', b'd', b'!'
+            ]),
+            Ok((&b""[..], string))
+        );
     }
 }
